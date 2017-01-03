685ee2a62ece53d87785745af602d633d9b0d3c9
totorowendy <totorowendychen@gmail.com>
2017-01-02 21:15:41 -0600

    remove _site from vs

--- _site/pull-content.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-blogdir=/mnt/c/Users/TotoroMuscles/Documents/blog/
-
-rm -rf ./_posts
-cp -r "$blogdir"/posts ./_posts
-
-rm -rf ./web-dist/images
-cp -r "$blogdir"/images ./web-dist/images
