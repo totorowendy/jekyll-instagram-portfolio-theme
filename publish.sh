5e2b9ca4080350ecc82a352dcb89909503f6be84
totorowendy <totorowendychen@gmail.com>
2017-01-02 21:16:24 -0600

    publishing new content

--- publish.sh
+++ publish.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-git add _site _posts web-dist/images
+git add _posts
+git commit -m "publishing new content"
+git push origin master
+rm -rf ../_site
+rm -rf ../vendor
+rm -rf ../.bundle
+mv vendor ..
+mv .bundle ..
+cp -r _site ../_site
+git checkout gh-pages
+rm -rf *
+mv ../_site/* .
+rm -rf ../_site
+git add .
 git commit -m "publishing new content"
 git push origin gh-pages
+git checkout master
+mv ../.bundle .
+mv ../vendor .
