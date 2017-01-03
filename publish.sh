844cfb31c74d91dd81b593c40764915ffcf72761
totorowendy <totorowendychen@gmail.com>
2017-01-02 21:05:30 -0600

    publish

--- publish.sh
+++ publish.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-git add _posts
-git commit -m "publishing new content"
-git push origin master
-cp -r _site ../_site
-git checkout gh-pages
-mv ../_site/* .
-rm -rf ../_site
-git add .
+git add _site _posts web-dist/images
 git commit -m "publishing new content"
 git push origin gh-pages
-git checkout master
-git reset --hard HEAD
